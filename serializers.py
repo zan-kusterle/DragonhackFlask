64dd15b98ab22154374c7f7b3ef7e6744517f8f7
Zan Kusterle <zan.kusterle@gmail.com>
2016-05-15 05:16:58 +0200

    fix

--- serializers.py
+++ serializers.py
@@ -1,5 +1,6 @@
 def serialize_result(r):
     return {
+        'id': str(r.id),
         'challenge_id': r.challenge_id,
         'user_id': r.user_id,
         'score': r.score,
@@ -9,6 +10,7 @@ def serialize_result(r):
 
 def serialize_challenge(c):
     return {
+        'id': str(c.id),
         'from_user_id': c.from_user_id,
         'to_user_id': c.to_user_id,
         'pitches_by_time': c.pitches_by_time
